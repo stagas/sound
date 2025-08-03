7e01d7c3498858daa4d587682c4dcd6fa8f74b55
stagas <gstagas@gmail.com>
2025-08-03 12:33:32 +0300

    save

--- demo6.js
+++ demo6.js
@@ -6,5 +6,5 @@ const chord = chords.pick(adv(1/4,sync(2)))
 const note = chord[0].ntof(1)
 const bass = lp(tanh(tri(note), 5.7),240,1.85)*.15*ar(.3,.01,sync(1/2,.25))
 const pad = bp(bp(chord.ntof(2).chord(saw)*ar(.7,0.4,sync(1)),250,3),100,1)*.4
-out = compressor(kick, -30, 25, .01, .04, 10) * 1.5 + bass + pad + hihat
+out = compressor(ls(reverb(compressor(kick,-30,25,.01,.04,10)*1.5 + bass + pad + hihat,.5,.5,.75,.5,.1),200,.5,-18),-30,25,.01,.04,10)*9
 `
